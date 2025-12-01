5feb324f50b3e319a65c85df367df89b3b2afe18
brycegoh <brycegoh96@gmail.com>
2025-12-01 12:42:44 +0800

    added checks and throw

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -1,19 +1,20 @@
 import { createClient } from "@supabase/supabase-js";
 
-// const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-// const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
-
 const env = (globalThis as any)?.process?.env ?? {};
 
 const supabaseUrl = 
-  env.NEXT_PUBLIC_SUPABASE_URL ||
-  env.SUPABASE_URL;
+  env.SUPABASE_URL ||
+  env.NEXT_PUBLIC_SUPABASE_URL;
 
 const supabaseAnonKey = 
-  env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  env.SUPABASE_ANON_KEY;
+  env.SUPABASE_ANON_KEY ||
+  env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
+console.log(`supabaseUrl: ${supabaseUrl}, supabaseAnonKey: ${supabaseAnonKey}`)
 
-console.log(supabaseUrl, supabaseAnonKey);
+if (!supabaseUrl || !supabaseAnonKey) {
+  throw new Error("SUPABASE_URL and SUPABASE_ANON_KEY are required");
+}
+
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey);
