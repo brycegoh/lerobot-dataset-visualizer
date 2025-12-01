bc3ae05c5a4553ab038f5289ec670c83017cfd82
rice1930 <owenyeo35@gmail.com>
2025-12-01 14:56:02 +0800

    public keys

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -5,12 +5,14 @@ const env = (globalThis as any)?.process?.env ?? {};
 const supabaseUrl = 
   env.SUPABASE_URL ||
   process.env.NEXT_PUBLIC_SUPABASE_URL ||
-  env.NEXT_PUBLIC_SUPABASE_URL;
+  env.NEXT_PUBLIC_SUPABASE_URL ||
+  "https://wkmnlrsvooiefqfauoud.supabase.co";
 
 const supabaseAnonKey = 
   env.SUPABASE_ANON_KEY ||
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
-  env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+  env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
+  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndrbW5scnN2b29pZWZxZmF1b3VkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0MzY1ODQsImV4cCI6MjA3OTAxMjU4NH0.D201wcBxfgDFDIR8YX4Ms-ZvFkPCWX9dqBFBqI0KvjQ";
 
 // console.log(`supabaseUrl: ${supabaseUrl}, supabaseAnonKey: ${supabaseAnonKey}`)
 
