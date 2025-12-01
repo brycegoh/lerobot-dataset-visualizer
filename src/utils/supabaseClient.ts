4809f097ee212180efbfb2dea6ad050db003307b
rice1930 <owenyeo35@gmail.com>
2025-12-01 12:58:42 +0800

    ckpt

--- src/utils/supabaseClient.ts
+++ src/utils/supabaseClient.ts
@@ -4,13 +4,15 @@ const env = (globalThis as any)?.process?.env ?? {};
 
 const supabaseUrl = 
   env.SUPABASE_URL ||
+  process.env.NEXT_PUBLIC_SUPABASE_URL ||
   env.NEXT_PUBLIC_SUPABASE_URL;
 
 const supabaseAnonKey = 
   env.SUPABASE_ANON_KEY ||
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY ||
   env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-console.log(`supabaseUrl: ${supabaseUrl}, supabaseAnonKey: ${supabaseAnonKey}`)
+// console.log(`supabaseUrl: ${supabaseUrl}, supabaseAnonKey: ${supabaseAnonKey}`)
 
 if (!supabaseUrl || !supabaseAnonKey) {
   throw new Error("SUPABASE_URL and SUPABASE_ANON_KEY are required");
